43d3c01d78ef32b3e8acec84a6f0851df5bd3a54
wberlinic <wyatt.berlinic@gmail.com>
2014-02-03 23:15:40 -0600

    Adding files for location tracking.

---
+++ BaseStation/src/tracker/UartListener.java
@@ -0,0 +1,14 @@
+package tracker;
+
+import com.pi4j.io.serial.SerialDataEvent;
+import com.pi4j.io.serial.SerialDataListener;
+
+public class UartListener implements SerialDataListener {
+
+	@Override
+	public void dataReceived(SerialDataEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
