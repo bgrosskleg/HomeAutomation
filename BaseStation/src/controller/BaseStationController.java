09e500378492cf1a7a491d6f5f4b43ec2a3154dd
Brian Grosskleg <Brian Grosskleg@10.227.5.231>
2014-02-27 17:56:53 -0600

    Changed BAUD rate back to 9600

--- BaseStation/src/controller/BaseStationController.java
+++ BaseStation/src/controller/BaseStationController.java
@@ -103,7 +103,7 @@ public class BaseStationController extends GenericController implements ModelSub
 			
 	
 	        // open the default serial port provided on the GPIO header
-	        serial.open(Serial.DEFAULT_COM_PORT, 1200);
+	        serial.open(Serial.DEFAULT_COM_PORT, 9600);
 	        
 	        // wait 1 second before continuing
 			Thread.sleep(1000);        
